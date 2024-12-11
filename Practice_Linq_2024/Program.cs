07833542b1e9efdf0c18b65c77d47d34c1139d20
Max <maksihnatyev.work@gmail.com>
2024-12-11 12:44:42 +0200

    Query №3

--- Practice_Linq_2024/Program.cs
+++ Practice_Linq_2024/Program.cs
@@ -92,14 +92,17 @@ namespace Practice_Linq_2024
         {
             //Query 3: Вивести всі домашні матчі збірної Франції за 2021 рік, де вона зіграла у нічию.
 
-            var selectedGames = games;   // Корегуємо запит !!!
+            var selectedGames = games.Where(e => e.Home_team == "France"
+                                                 && e.Date.Year == 2021
+                                                 && e.Country == "France"
+                                                 && (e.Home_score == e.Away_score));   // Корегуємо запит !!!
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 3 ========================");
 
             // див. приклад як має бути виведено:
 
-
+            PrintGames(selectedGames);
         }
 
         // Запит 4
